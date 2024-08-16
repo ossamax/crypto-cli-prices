7a842efde15ce8938483bdaf2862de878005a729
ossama zaoute <ramziioussama@gmail.com>
2024-08-16 14:44:36 +0100

    finished

--- bin/coindex-key.js
+++ bin/coindex-key.js
@@ -3,7 +3,7 @@ const key = require("../commands/keys");
 
 program
   .command("set")
-  .description("Set API key -- get at https://nomics.com")
+  .description("Set API key -- get at https://example.com")
   .action(key.set);
 
 program.command("show").description("Show API key ").action(key.show);
